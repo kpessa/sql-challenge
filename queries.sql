583c2d0cb3c8d2e1ed99a0e6ec087d69efd332e1
Kurt Pessa <kpessa@gmail.com>
2020-08-10 18:41:10 -0400

    finished 8 questions for data analysis and SQL queries

--- queries.sql
+++ queries.sql
@@ -18,3 +18,12 @@ SELECT e.emp_no, last_name, first_name, dept_name FROM employees as e
 JOIN dept_emp as de on e.emp_no = de.emp_no
 JOIN departments as d on de.dept_no = d.dept_no
 WHERE dept_name = 'Sales'
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, last_name, first_name, dept_name FROM employees as e
+JOIN dept_emp as de on e.emp_no = de.emp_no
+JOIN departments as d on de.dept_no = d.dept_no
+WHERE dept_name = 'Sales' or dept_name = 'Development'
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) as frequency FROM employees 
+GROUP BY last_name
+ORDER BY frequency desc
